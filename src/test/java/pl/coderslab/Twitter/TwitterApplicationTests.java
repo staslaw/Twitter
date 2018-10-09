fc5dc94b4ff34f8aa553dbcb39de2baf64e3e58c
Stanislaw <sniemyski@o2.pl>
2018-10-09 22:08:52 +0200

    settings

--- src/test/java/pl/coderslab/Twitter/TwitterApplicationTests.java
+++ src/test/java/pl/coderslab/Twitter/TwitterApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.coderslab.sor;
+package pl.coderslab.Twitter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SorApplicationTests {
+public class TwitterApplicationTests {
 
     @Test
     public void contextLoads() {
